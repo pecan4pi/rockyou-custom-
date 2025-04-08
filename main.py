4f34c4d548502c59f272b5e56cf32e7e087d5bbc
pecan4pi <157906054+pecan4pi@users.noreply.github.com>
2025-04-08 15:04:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import linecache
+import time
+import pywifi
+from pywifi import const
 
-line = linecache.getline('rockyou.txt', 4)
+def connect_to_wifi(ssid, password):
+    wifi = pywifi.PyWiFi()
+    iface = wifi.interfaces()[0]
+    iface.disconnect()
+    time.sleep(1)
+    if iface.status() == const.IFACE_DISCONNECTED:
+        profile = pywifi.Profile()
+        profile.ssid = ssid
+        profile.key = password
+        profile.auth = const.AUTH_ALG_OPEN
+        profile.akm.append(const.AKM_TYPE_WPA2PSK)
+        profile.cipher = const.CIPHER_TYPE_CCMP
+        iface.remove_all_network_profiles()
+        tmp_profile = iface.add_network_profile(profile)
+        iface.connect(tmp_profile)
+        time.sleep(10)
+        if iface.status() == const.IFACE_CONNECTED:
+            print("pw found")
+        else:
 
-print(line)
+# Example usage
+connect_to_wifi("Your_SSID", "Your_Password")
+
